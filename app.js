65840c423cf2518701ac0f864dd564b67317e279
emmeiwhite <emmeiwhite@gmail.com>
2024-10-27 22:28:32 +0530

    Route for logo.svg resource added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { readFileSync, read } = require('fs')
 // get all files
 const homePage = readFileSync('./navbar-app/index.html')
 const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
 
 const server = http.createServer((request, response) => {
   const url = request.url
@@ -16,21 +17,35 @@ const server = http.createServer((request, response) => {
     })
     response.write(homePage)
     response.end()
-  } else if (url === '/styles.css') {
-    // About Page
+  }
+  // Styles
+  else if (url === '/styles.css') {
     response.writeHead(200, {
       'content-type': 'text/css'
     })
     response.write(homeStyles)
     response.end()
-  } else if (url === '/about') {
-    // About Page
+  }
+  // Logo
+  else if (url === '/logo.svg') {
+    response.writeHead(200, {
+      'content-type': 'image/svg+xml'
+    })
+    response.write(homeImage)
+    response.end()
+  }
+
+  // About Page
+  else if (url === '/about') {
     response.writeHead(200, {
       'content-type': 'text/html'
     })
     response.write('<h1>The About Page</h1>')
     response.end()
-  } else {
+  }
+
+  //   Error Page
+  else {
     response.writeHead(404, {
       'content-type': 'text/html'
     })
