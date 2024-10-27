31ca1c3f0a503accfa276e1ad2d79042c09eecaa
emmeiwhite <emmeiwhite@gmail.com>
2024-10-27 21:09:14 +0530

    Handling requests to send back appropriate responses!

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ const http = require('http')
 const server = http.createServer((request, response) => {
   const url = request.url
   console.log(url)
-  response.writeHead(200, {
-    'content-type': 'text/html'
-  })
-  response.write('<h1>Dear Browser, render me as HTML5</h1>')
-  response.end()
+
+  if (url === '/') {
+    // Homepage
+    response.writeHead(200, {
+      'content-type': 'text/html'
+    })
+    response.write('<h1>The Home Page</h1>')
+    response.end()
+  } else if (url === '/about') {
+    // About Page
+    response.writeHead(200, {
+      'content-type': 'text/html'
+    })
+    response.write('<h1>The About Page</h1>')
+    response.end()
+  } else {
+    response.writeHead(404, {
+      'content-type': 'text/html'
+    })
+    response.write('<h1>Page not Found!!</h1>')
+    response.end()
+  }
 })
 
 server.listen(5000)
