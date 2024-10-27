49a4686a5331c3ae2663b166d0bc7c9d7f23a7b1
emmeiwhite <emmeiwhite@gmail.com>
2024-10-27 16:17:50 +0530

    Response Headers: 'content-type':'text/html'

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http')
 
 const server = http.createServer((request, response) => {
-  console.log('User hit the server')
-  response.end('Home Page')
+  response.writeHead(200, {
+    'content-type': 'text/html'
+  })
+  response.write('<h1>Dear Browser, render me as HTML5</h1>')
+  response.end()
 })
 
 server.listen(5000)
