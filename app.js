fff358fafb2facbbff7f1451d86b9a84ef1902ec
emmeiwhite <emmeiwhite@gmail.com>
2024-10-28 22:21:30 +0530

    We return only fields required from products with de-structuring

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 
 const { products } = require('./data')
+
 app.get('/', (req, res) => {
-  res.json(products)
+  res.send(`<div>
+                    <h1>Home Page</h1>
+                    <a href="/api/products">Products</a>
+        </div>`)
+})
+
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map(({ id, name, image }) => {
+    return { id, name, image }
+  })
+
+  res.json(newProducts)
+  //   res.send('products')
 })
 
 app.listen(5000, () => {
