63e7f9dc4601fe49830311e4bac5d1c2f19ad8a4
emmeiwhite <emmeiwhite@gmail.com>
2024-10-28 15:10:34 +0530

    res.status(404).send('resource not found')

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express')
 // Right away we get the server!
 const app = express()
 
+app.get('/', (req, res) => {
+  console.log('Home Page')
+  res.send('Home Page')
+})
+
 app.listen(5000, () => {
-  console.log('app is listening!!!')
+  console.log('server is listening on port 5000')
 })
