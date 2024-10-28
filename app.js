8ca227f6a335fe8fd41100edc67c473d85e962bb
emmeiwhite <emmeiwhite@gmail.com>
2024-10-28 15:51:17 +0530

    100 % static side served now from the server.

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const app = express()
 // static setup & middleware
 app.use(express.static('./public'))
 
+/* ---
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
 
+// We actually do not need to send static assest index.html exclusively, since it is a static asset, we simply also keep it in the public folder and life is sorted
+--- */
 app.all('*', (req, res) => {
   res.status(404).send('<h1>Resource not found!</h1>')
 })
