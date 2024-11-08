4ff07e272064d3b912c689aa0cfde7561912e17a
emmeiwhite <emmeiwhite@gmail.com>
2024-11-08 20:06:35 +0530

    Basic express setup

---
+++ app-v5.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const http = require('http')
+// const app = express()
+
+const server = http.createServer((req, res) => {
+  // Monolithic Request Handler Function
+  if (req.url === '/') {
+    res.writeHead(200)
+    res.write('<h1>Homepage !!!</h1>')
+    res.end()
+  }
+})
+
+server.listen(5000, () => {
+  console.log('App is listening on port 5000')
+})
+// app.listen(5000, () => {
+//   console.log('App is listening on port 5000')
+// })
--- app.js
+++ app.js
@@ -1,19 +1,7 @@
 const express = require('express')
-const http = require('http')
-// const app = express()
 
-const server = http.createServer((req, res) => {
-  // Monolithic Request Handler Function
-  if (req.url === '/') {
-    res.writeHead(200)
-    res.write('<h1>Homepage !!!</h1>')
-    res.end()
-  }
-})
+const app = express()
 
-server.listen(5000, () => {
-  console.log('App is listening on port 5000')
+app.listen(5000, () => {
+  console.log('Server is running on port 5000')
 })
-// app.listen(5000, () => {
-//   console.log('App is listening on port 5000')
-// })
