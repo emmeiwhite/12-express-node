7c8361889cb7c53c3813f74ec6283d3d358df744
emmeiwhite <emmeiwhite@gmail.com>
2024-11-10 14:34:28 +0530

    node-1: globals - we access anywhere within application

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 
 const app = express()
 
+/* --- Let's create two routes --- */
+
+app.get('/', (req, res) => {
+  res.send('<h1>WELCOME TO THE HOMEPAGE!</h1>')
+})
+
+app.get('/about', (req, res) => {
+  res.send('<h1>ABOUT REST API</h1>')
+})
+
 app.listen(5000, () => {
   console.log('Server is running on port 5000')
 })
---
+++ node-foundations/1.globals.js
@@ -0,0 +1,9 @@
+// globals are executed anywhere in the application, whether we are deeply nested or anywhere for that matter.
+
+/**
+ * 1. __dirname:Currently working directly
+ * 2. __filename: Currently executing file name
+ * 3. require: To import a module
+ * 4. modules: To export values from a module
+ * 5. process: To get the environment details
+ */
